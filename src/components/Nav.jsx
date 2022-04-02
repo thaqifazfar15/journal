11f9581567a54ca3985a409f0ce9de23924976c2
thaqifazfar15 <thaqifazfar15@gmail.com>
2022-04-02 20:16:29 +0800

    Finish Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Nav() {
   let Links = [
@@ -8,18 +8,27 @@ function Nav() {
     { name: "CONTACT", link: "/" },
   ];
 
+  let [open, setOpen] = useState(false);
+
   return (
     <div className="fixed top-0 left-0 w-full shadow-md">
-      <div className="items-center justify-between bg-white py-5 px-5 md:flex md:px-20">
+      <div className="items-center justify-between bg-white py-5 px-5 md:flex md:px-12">
         <div className="flex cursor-pointer items-center text-2xl font-bold text-gray-800 md:mb-0">
           Journal
         </div>
 
-        <div className="absolute right-8 top-6 cursor-pointer text-3xl md:hidden">
-          <ion-icon name="menu-outline"></ion-icon>
+        <div
+          onClick={() => setOpen(!open)}
+          className="absolute right-8 top-6 cursor-pointer text-3xl md:hidden"
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
         </div>
 
-        <ul className="absolute left-0 z-[-1] flex w-full flex-col bg-white py-4 pl-5 duration-500 ease-in md:static md:z-auto md:w-auto md:flex-row md:items-center md:py-0 md:pl-0">
+        <ul
+          className={`absolute left-0 z-[-1] flex w-full flex-col bg-white py-4 pl-5 opacity-0 duration-500 ease-in md:static md:z-auto md:w-auto md:flex-row md:items-center md:py-0 md:pl-0 md:opacity-100 ${
+            open ? "top-[4.6rem] opacity-100" : "top-[-490px]"
+          }`}
+        >
           {Links.map((Link) => (
             <li
               key={Link.name}
