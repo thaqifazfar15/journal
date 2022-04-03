cd79fe0ffd24cd1f742d6bac2cc0d0cb0c3ea3a3
thaqifazfar15 <thaqifazfar15@gmail.com>
2022-04-03 17:24:13 +0800

    Change font size

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,15 +5,21 @@ function Main() {
     <div className="flex h-[100%] flex-col items-center justify-center">
       {/* Container */}
       <div className="inter-font flex h-full max-w-[1200px] flex-col items-start justify-center px-7 pb-12 md:pb-4">
-        <p className="inter-font md:text-md text-2xs text-gray-600">
+        <p className="inter-font text-md text-gray-600 md:text-xl">
           Hi there, welcome to my
         </p>
-        <h1 className="text-4xl font-bold text-slate-100 md:text-6xl">
+        <h1 className="text-4xl font-bold text-slate-100 md:text-7xl">
           Software Engineer
         </h1>
-        <h2 className="text-4xl font-bold text-lime-300 md:text-6xl">
+        <h2 className="text-4xl font-bold text-lime-300 md:text-7xl">
           Study Journal
-          <p>of 2022.</p>
+          <p>
+            of{" "}
+            <span className="duration-300 hover:text-5xl md:hover:text-8xl">
+              2022
+            </span>
+            .
+          </p>
         </h2>
         <button className="my-3 border-2 px-6 py-3 text-white hover:border-pink-500 hover:bg-pink-500 hover:shadow-md">
           View Blog
