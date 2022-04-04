6c4389b9522c1519abb153bab5ef65839c14b2cf
thaqifazfar15 <thaqifazfar15@gmail.com>
2022-04-04 23:42:51 +0800

    change font size

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="flex h-[100%] flex-col items-center justify-center">
+    <div className="flex h-[100%] w-full flex-col items-center justify-center">
       {/* Container */}
       <div className="inter-font flex h-full max-w-[1200px] flex-col items-start justify-center px-7 pb-12 md:pb-4">
         <p className="inter-font text-md text-gray-600 md:text-xl">
@@ -14,11 +14,7 @@ function Main() {
         <h2 className="text-4xl font-bold text-lime-300 md:text-7xl">
           Study Journal
           <p>
-            of{" "}
-            <span className="duration-300 hover:text-5xl md:hover:text-8xl">
-              2022
-            </span>
-            .
+            of <span className="text-5xl md:text-8xl">2022</span>.
           </p>
         </h2>
         <button className="my-3 border-2 px-6 py-3 text-white hover:border-pink-500 hover:bg-pink-500 hover:shadow-md">
