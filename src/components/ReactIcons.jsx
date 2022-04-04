cf85e1a485d7968b342e61589306b140045d8484
thaqifazfar15 <thaqifazfar15@gmail.com>
2022-04-04 23:41:58 +0800

    feat: make icons responsive

--- src/components/ReactIcons.jsx
+++ src/components/ReactIcons.jsx
@@ -11,25 +11,26 @@ import { GrMysql } from "react-icons/gr";
 
 function ReactIcons() {
   let Icons = [{ logo: "FaRust" }, { logo: "SiWeb3Dotjs" }];
+  let hello = "SiElixir";
   return (
     <div className="contents">
-      <div className="grid h-20  w-20 place-items-center shadow-lg">
-        <FaRust className="text-[3.5rem] text-gray-400" />
+      <div className="grid h-20  w-20 place-items-center shadow-lg md:h-[7rem] md:w-[7rem]">
+        <FaRust className="text-[3.5rem] text-gray-400 hover:text-gray-500 md:text-[5.2rem] " />
       </div>
-      <div className="grid h-20  w-20 place-items-center shadow-lg">
-        <SiWeb3Dotjs className="text-[3.5rem] text-gray-400" />
+      <div className="grid h-20  w-20 place-items-center shadow-lg md:h-[7rem] md:w-[7rem]">
+        <SiWeb3Dotjs className="text-[3.5rem] text-gray-400 hover:text-gray-500 md:text-[5.2rem]" />
       </div>
-      <div className="grid h-20  w-20 place-items-center shadow-lg">
-        <SiCplusplus className="text-[3.5rem] text-gray-400" />
+      <div className="grid h-20 w-20 place-items-center shadow-lg md:h-[7rem] md:w-[7rem]">
+        <SiCplusplus className="text-[3.5rem] text-gray-400 hover:text-gray-500 md:text-[5.1rem]" />
       </div>
-      <div className="grid h-20  w-20 place-items-center shadow-lg">
-        <SiNextdotjs className="text-[3.5rem] text-gray-400" />
+      <div className="grid h-20 w-20 place-items-center shadow-lg md:h-[7rem] md:w-[7rem]">
+        <SiNextdotjs className="text-[3.5rem] text-gray-400 hover:text-gray-500 md:text-[5rem]" />
       </div>
-      <div className="grid h-20  w-20 place-items-center shadow-lg">
-        <SiElixir className="text-[3.5rem] text-gray-400" />
+      <div className="grid h-20 w-20 place-items-center shadow-lg md:h-[7rem] md:w-[7rem]">
+        <SiElixir className="text-[3.5rem] text-gray-400 hover:text-gray-500 md:text-[5rem]" />
       </div>
-      <div className="grid h-20  w-20 place-items-center shadow-lg">
-        <GrMysql className="text-[3.5rem] text-gray-400" />
+      <div className="grid h-20 w-20 place-items-center shadow-lg md:h-[7rem] md:w-[7rem]">
+        <GrMysql className="text-[3.5rem] text-gray-400 hover:text-gray-500 md:text-[5.2rem]" />
       </div>
     </div>
   );
