c39dc6b5653e96a90ecf8f29b7530a39453abbfa
thaqifazfar15 <thaqifazfar15@gmail.com>
2022-04-04 17:50:34 +0800

    Add <Tech/> component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
+import Tech from "./components/Tech";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="h-screen w-full bg-indigo-400 pt-[80px]">
         <Main />
       </div>
+      <Tech />
     </>
   );
 }
